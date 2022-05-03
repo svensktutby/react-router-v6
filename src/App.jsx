77789501160adb9678279eb9c1006932845919a9
Andrei Shved <svensk.tut.by@gmail.com>
2022-05-03 22:40:39 +0300

    feat: add Layout and Outlet components

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,21 @@
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 import { Home } from './pages/Home';
 import { About } from './pages/About';
 import { Blog } from './pages/Blog';
 import { NotFound } from './pages/NotFound';
+import { Layout } from './components/Layout';
 
 function App() {
     return (
         <>
-            <header>
-                <Link to="/">Home</Link>
-                <Link to="/posts">Blog</Link>
-                <Link to="/about">About</Link>
-            </header>
             <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/posts" element={<Blog />} />
-                <Route path="*" element={<NotFound />} />
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<Home />} />
+                    <Route path="about" element={<About />} />
+                    <Route path="posts" element={<Blog />} />
+                    <Route path="*" element={<NotFound />} />
+                </Route>
             </Routes>
         </>
     );
