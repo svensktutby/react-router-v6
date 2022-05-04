4e6f989a20d9210cc2e82ab67a1fd42f1a80c07b
Andrei Shved <svensk.tut.by@gmail.com>
2022-05-04 22:28:24 +0300

    feat: add CustomLink instead of NavLink

---
+++ src/components/CustomLink.jsx
@@ -0,0 +1,13 @@
+import { Link, useMatch } from 'react-router-dom';
+
+const CustomLink = ({ children, to, ...props }) => {
+    const match = useMatch(to);
+
+    return (
+        <Link to={to} style={{ color: match ? 'var(--color-active)' : '#fff' }} {...props}>
+            {children}
+        </Link>
+    );
+};
+
+export { CustomLink };
